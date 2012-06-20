d88e50207f66d15f1d5355e8b0f9a7bb07b54e1f
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-06-20 10:56:57 -0300

    Reorganize directories (everything is in root)

--- tokenizer.py
+++ tokenizer.py
@@ -7,5 +7,6 @@ import re
 regexp_split = re.compile('([ \t\r\n{}])'.format(re.escape(string.punctuation)))
 
 def tokenize(text):
+    #TODO: add option to specify stopwords
     splitted_text = regexp_split.split(text)
     return [token for token in splitted_text if token.strip()]
