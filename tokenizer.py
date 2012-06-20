0c9da2eb82954288d7d53e2e8426cc818fe46e7a
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-06-20 09:57:45 -0300

    Add tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import string
+import re
+
+
+regexp_split = re.compile('([ \t\r\n{}])'.format(re.escape(string.punctuation)))
+
+def tokenize(text):
+    splitted_text = regexp_split.split(text)
+    return [token for token in splitted_text if token.strip()]
